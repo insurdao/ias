86275c8fc248dee770df90db965a6bc955074113
Henry Hazan <henry@wepy.io>
2021-02-19 11:27:25 +0200

    add

--- src/claim.sol
+++ src/claim.sol
@@ -6,6 +6,17 @@ pragma solidity >=0.8.0;
 import "ds-auth/auth.sol";
 
 contract Claim is DSAuth {
+
+    // --- Auth ---
+    mapping (address => uint256) public wards;
+    function rely(address usr) external note auth { wards[usr] = 1; }
+    function deny(address usr) external note auth { wards[usr] = 0; }
+    modifier auth {
+        require(wards[msg.sender] == 1, "claim/not-authorized");
+        _;
+    }
+
+    // --- Data ---
     string  uuid;
     bytes32 group;
     address mediator;
--- src/mutual.sol
+++ src/mutual.sol
@@ -4,6 +4,9 @@ pragma solidity >=0.8.0;
 
 import "ds-auth/auth.sol";
 
+
+interface Policy {}
+
 contract Mutual is DSAuth {
 
 
@@ -30,5 +33,24 @@ contract Mutual is DSAuth {
 
   }
 
+  /// have exclusive functions for that.
+  /// @param name group name
+  /// @param token the token address, can be USDT, etc..
+  /// @param policy name of the policy template we want for this group
+  function createGroup(string calldata name, string calldata policy, address token) external {
+    bytes32 hashedGroup = keccak256(abi.encodePacked(name));
+    bytes32 hashedPolicy = keccak256(abi.encodePacked(policy));
+
+    require(hasRole(DEFAULT_ADMIN_ROLE, msg.sender), "Caller is not an admin");
+    // require(groups(hashedGroup).length == 0, "Already added group");
+
+    Group storage group = groups[hashedGroup];
+    group.owner = msg.sender;
+    group.manager = msg.sender;
+    group.token = token;
+    group.name = name;
+    group.timestamp = block.timestamp;
+    group.policy = policies[hashedPolicy];
+  }
 
 }
