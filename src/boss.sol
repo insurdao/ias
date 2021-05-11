66bc3bc6f7f16b8a79f1486ec4fc8fcae6d15617
Henry Hazan <henry@insurdao.com>
2021-05-11 08:51:16 +0300

    add math

--- src/boss.sol
+++ src/boss.sol
@@ -4,6 +4,7 @@
 pragma solidity >=0.8.0;
 
 import "ds-auth/auth.sol";
+import "ds-note/note.sol";
 
 
 interface PolicyLike {
@@ -13,7 +14,12 @@ interface PolicyLike {
 
 interface ClaimLike {}
 
-contract Boss is DSAuth {
+contract Boss is DSAuth, DSNote {
+  mapping (bytes32 => address) public policies;
+  mapping (address => mapping (bytes32 => mapping (address => bool))) public allows;
+  uint256 public policyId;
+
+  // event NewPolicy(address indexed guy, bytes12 cdp);
 
   uint                      public poli;      // Auto incremental
   mapping (uint => address) public owns;      // PolicyId => Owner
@@ -25,4 +31,18 @@ contract Boss is DSAuth {
   }
 
 
+  function open(address token, address usr) public note returns (uint) {
+    require(usr != address(0), "usr-address-0");
+
+  }
+
+
+  // --- Math ---
+  function add(uint x, uint y) internal pure returns (uint z) {
+      require((z = x + y) >= x);
+  }
+
+  function sub(uint x, uint y) internal pure returns (uint z) {
+      require((z = x - y) <= x);
+  }
 }
