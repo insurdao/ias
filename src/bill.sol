ee6c5ffca2c53fa44eebafc63f82ed3067c8fdeb
Henry Hazan <henry@teramine.io>
2021-11-07 12:14:24 +0200

    clean

--- src/bill.sol
+++
@@ -1,3 +0,0 @@
-// SPDX-License-Identifier: MIT
-pragma solidity >=0.8.0;
-/// Implement the different ways to send premium to the mutual
---
+++ src/seal.sol
@@ -0,0 +1,13 @@
+// SPDX-License-Identifier: MIT
+pragma solidity >=0.8.0;
+
+/// Underwriting acceptance. Here we will authorize a new member to join, and keep track
+/// his own intrinsic risk, related to a specific policy, so we keep the insured relation
+/// between the vault, policy, risk and underwriting.
+/// States: ACCEPTED, ACTIVE, BLACK_LISTED, EXPIRED
+
+
+contract Seal {
+
+
+}
